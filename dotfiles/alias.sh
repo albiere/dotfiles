d9e3746a77d5fc5994c2198c773a3f12e9d18b73
Angelo Albiero Neto <angeloalbiero@gmail.com>
2015-04-24 22:54:16 +1200

    Add PS and DF aliases

--- dotfiles/alias.sh
+++ dotfiles/alias.sh
@@ -1,3 +1,11 @@
 alias ls='ls -Glh'
 alias lsa='ls -Glha'
 alias reload='source ~/.bash_profile'
+
+# PS
+alias psa="ps aux"
+alias psg="ps aux | grep "
+alias psr='ps aux | grep ruby'
+
+# DF
+alias df='df -h'
