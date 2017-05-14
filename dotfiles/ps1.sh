2b5885704f6b07146ec662be037b2c4102a2799c
Angelo Albiero Neto <angeloalbiero@gmail.com>
2017-05-14 01:01:43 -0400

    Add k8s context to PS1

--- dotfiles/ps1.sh
+++ dotfiles/ps1.sh
@@ -1,2 +1,10 @@
+# LIST COLORS
+# for code in {0..255}
+# do echo -e "\e[38;5;${code}m"'\\e[38;5;'"$code"m"\e[0m"
+# done
+
+KUBE_CONTEXT=$(kubectl config current-context | xargs printf "(k8s:%s)")
+GIT_BRANCH=$(__git_ps1 '(git:%s)')
+
 PS1="\n\w\n$ "
-export PS1="\n\[\e[0;33m\]\w\[\e[0;91m\]\$(__git_ps1 ' (%s)')\[\e[0;96m\]\n$ \[\e[0m\]"
+export PS1="\n\[\e[0;33m\]\w \[\e[38;5;13m\]\$KUBE_CONTEXT \[\e[0;91m\]\$GIT_BRANCH\[\e[0;96m\]\n$ \[\e[0m\]"
