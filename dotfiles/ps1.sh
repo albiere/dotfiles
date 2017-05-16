138519c0e941b4444a6f569f4e0c3755c4691c30
Angelo Albiero Neto <angeloalbiero@gmail.com>
2017-05-16 10:43:26 -0400

    Fix PS1

--- dotfiles/ps1.sh
+++ dotfiles/ps1.sh
@@ -3,8 +3,13 @@
 # do echo -e "\e[38;5;${code}m"'\\e[38;5;'"$code"m"\e[0m"
 # done
 
-KUBE_CONTEXT=$(kubectl config current-context | xargs printf "(k8s:%s)")
-GIT_BRANCH=$(__git_ps1 '(git:%s)')
+function kube_context {
+  echo $(kubectl config current-context | xargs printf "(k8s:%s)")
+}
+
+function git_branch {
+  echo $(__git_ps1 '(git:%s)')
+}
 
 PS1="\n\w\n$ "
-export PS1="\n\[\e[0;33m\]\w \[\e[38;5;13m\]\$KUBE_CONTEXT \[\e[0;91m\]\$GIT_BRANCH\[\e[0;96m\]\n$ \[\e[0m\]"
+export PS1="\n\[\e[0;33m\]\w \[\e[38;5;13m\]\$(kube_context) \[\e[0;91m\]\$(git_branch)\[\e[0;96m\]\n$ \[\e[0m\]"
