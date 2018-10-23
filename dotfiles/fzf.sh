47e9e12f7fe76b74caabf205c55748342dd6d389
Angelo Albiero Neto <angeloalbiero@gmail.com>
2018-10-23 15:19:16 -0400

    Add fzf config

---
+++ dotfiles/fzf.sh
@@ -0,0 +1,7 @@
+if which fd >/dev/null 2>&1; then
+  export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude ".git" .'
+elif which rg >/dev/null 2>&1; then
+  export FZF_DEFAULT_COMMAND='rg --files'
+else
+  export FZF_DEFAULT_COMMAND='find . -name .git -prune -o -print'
+fi
--- packages/packages.sh
+++ packages/packages.sh
@@ -1,5 +1,3 @@
 brew install watch
-
+brew install gron
 brew install fzf
-source /usr/local/opt/fzf/shell/completion.bash
-source /usr/local/opt/fzf/shell/key-bindings.bash
