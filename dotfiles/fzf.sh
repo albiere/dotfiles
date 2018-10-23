8fad489801be41d8df38c2f13aa100f5d0767819
Angelo Albiero Neto <angeloalbiero@gmail.com>
2018-10-23 15:37:01 -0400

    More FZF configuration

--- dotfiles/fzf.sh
+++ dotfiles/fzf.sh
@@ -1,3 +1,5 @@
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
 if which fd >/dev/null 2>&1; then
   export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude ".git" .'
 elif which rg >/dev/null 2>&1; then
--- packages/packages.sh
+++ packages/packages.sh
@@ -1,3 +1,6 @@
 brew install watch
 brew install gron
+
+brew install fd
 brew install fzf
+/usr/local/opt/fzf/install
