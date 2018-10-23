a131aa34d2d2b1141ae8fdb2735abfde1aae92b6
Angelo Albiero Neto <angeloalbiero@gmail.com>
2018-10-23 15:18:24 -0400

    Add GOPATH config

--- dotfiles/general.sh
+++ dotfiles/general.sh
@@ -1,6 +1,8 @@
 export HISTCONTROL=ignoredups
 export HISTTIMEFORMAT="%F %T "
 export EDITOR=vim
+export GOPATH=$(go env GOPATH)
+export PATH=$PATH:$GOPATH/bin
 
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
   . $(brew --prefix)/etc/bash_completion
