e3830c70c018466a98f215798698446c9c6aca6e
Angelo Albiero Neto <angeloalbiero@hey.com>
2020-09-12 11:43:20 -0400

    Add Tmux package manager

--- packages/packages.sh
+++ packages/packages.sh
@@ -1,3 +1,21 @@
-brew install bash-completion vim tmux curl wget nmap watch fd fzf
+#!/bin/bash
 
+# Brew packages
+brew install \
+  bash-completion \
+  vim \
+  tmux \
+  curl \
+  wget \
+  nmap \
+  watch \
+  fd \
+  fzf
+
+# Tmux package manager
+if [ -d "~/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+# Instal FZF
 /usr/local/opt/fzf/install
