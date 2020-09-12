a5f3f7ab4cd39bc134d69d54391ff57563f9c6e7
Angelo Albiero Neto <angeloalbiero@hey.com>
2020-09-12 11:14:02 -0400

    Tweak Rbenv

--- dotfiles/rbenv.sh
+++ dotfiles/rbenv.sh
@@ -1,2 +1,7 @@
 export PATH="$HOME/.rbenv/bin:$PATH"
+
+# To link Rubies to Homebrew's OpenSSL 1.1 (which is upgraded) add the following
+# to your /Users/angeloalbiero/.bash_profile:
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+
 eval "$(rbenv init -)"
