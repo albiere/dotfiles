0aaf9fe94799f2c33a2433ed1cc2d05759620bd0
Angelo Albiero Neto <angeloalbiero@hey.com>
2023-03-27 18:05:10 -0300

    Fix git clones and configure asdf

--- bash/dot-config/bash/asdf.sh
+++ bash/dot-config/bash/asdf.sh
@@ -1,2 +1,6 @@
-source $HOME/.asdf/asdf.sh
-source $HOME/.asdf/completions/asdf.bash
+source $HOME/.config/asdf/asdf.sh
+source $HOME/.config/asdf/completions/asdf.bash
+
+export ASDF_CONFIG_FILE=$HOME/.config/asdf/.asdfrc
+export ASDF_DIR=$HOME/.config/asdf
+export ASDF_DATA_DIR=$HOME/.config/asdf
