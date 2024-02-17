1416c4b59383a3680e076503019a2da57dafe1c9
Angelo Albiero Neto <angeloalbiero@hey.com>
2024-02-17 12:46:23 -0500

    Add extra snippets and tweak ui of completion box

--- nvim/dot-config/nvim/lua/albiere/plugins/luasnip.lua
+++ nvim/dot-config/nvim/lua/albiere/plugins/luasnip.lua
@@ -1,6 +1,13 @@
+local luasnip_status, luasnip = pcall(require, "luasnip")
+if not luasnip_status then
+	return
+end
+
 local luasnip_loaders_status, luasnip_loaders = pcall(require, "luasnip.loaders.from_vscode")
 if not luasnip_loaders_status then
 	return
 end
 
 luasnip_loaders.lazy_load()
+luasnip.filetype_extend("ruby", { "rails" })
+luasnip.filetype_extend("eruby", { "html" })
