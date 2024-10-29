1f858bb5e42f5b6cc27b71c540dc124cf71d3ac3
Angelo Albiero Neto <angeloalbiero@hey.com>
2024-10-29 11:13:12 -0400

    Update list of language servers

--- nvim/dot-config/nvim/lua/albiere/plugins/lsp/mason.lua
+++ nvim/dot-config/nvim/lua/albiere/plugins/lsp/mason.lua
@@ -1,30 +1,32 @@
 local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-	return
+  return
 end
 
 local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-	return
+  return
 end
 
 mason.setup({
-	ui = {
-		border = "single",
-		height = 0.8,
-		width = 0.8,
-	},
-	PATH = "append",
+  ui = {
+    border = "single",
+    height = 0.8,
+    width = 0.8,
+  },
+  PATH = "append",
 })
 
 mason_lspconfig.setup({
-	ensure_installed = {
-		"tsserver",
-		"cssls",
-		"tailwindcss",
-		"lua_ls",
-		"elixirls",
-		"rubocop",
-	},
-	automatic_installation = true,
+  ensure_installed = {
+    "emmet_ls",
+    "ts_ls",
+    "cssls",
+    "tailwindcss",
+    "ruby_lsp",
+    "elixirls",
+    "rubocop",
+    "lua_ls",
+  },
+  automatic_installation = true,
 })
