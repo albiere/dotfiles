8d2a896b7bad741d3a993cb43afab017705b614c
Angelo Albiero Neto <angeloalbiero@hey.com>
2025-06-05 15:31:13 -0400

    Install asdf via hombrew

--- bash/dot-config/bash/asdf.sh
+++ bash/dot-config/bash/asdf.sh
@@ -1,6 +1,8 @@
 source $HOME/.config/asdf/asdf.sh
 source $HOME/.config/asdf/completions/asdf.bash
 
-export ASDF_CONFIG_FILE=$HOME/.config/asdf/.asdfrc
 export ASDF_DIR=$HOME/.config/asdf
 export ASDF_DATA_DIR=$HOME/.config/asdf
+
+export PATH="/usr/local/opt/asdf/bin:$PATH"
+export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
