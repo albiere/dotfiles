537e8fb285fc5fb1cef5a0c4ee5a9c6d27d3a8a9
Angelo Albiero Neto <angeloalbiero@hey.com>
2025-06-12 15:21:35 -0400

    Update lsp diagnostics and refactor lspconfig

--- nvim/dot-config/nvim/lua/albiere/plugins/lsp/lspconfig.lua
+++ nvim/dot-config/nvim/lua/albiere/plugins/lsp/lspconfig.lua
@@ -1,31 +1,56 @@
 local keymap = vim.keymap
 
 vim.diagnostic.config({
+  -- virtual_lines = true,
+  virtual_text = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = 'rounded',
+    source = true,
+  },
   signs = {
     text = {
-      [vim.diagnostic.severity.ERROR] = "",
-      [vim.diagnostic.severity.WARN] = "",
-      [vim.diagnostic.severity.HINT] = "ﴞ",
-      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.ERROR] = "󰅚",
+      [vim.diagnostic.severity.WARN] = "󰀪",
+      [vim.diagnostic.severity.INFO] = "󰋽",
+      [vim.diagnostic.severity.HINT] = "󰌶",
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+      [vim.diagnostic.severity.WARN] = "WarningMsg",
     }
   }
 })
 
-function lsp_completion(ev)
+-- Override the default hover function to use a custom border and size
+-- global winborder breaks Telescope, so we use a custom hover function instead
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or "rounded"
+  return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
+local function lsp_completion(ev)
   local client = vim.lsp.get_client_by_id(ev.data.client_id)
   if client:supports_method('textDocument/completion') then
     vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
   end
 end
 
-function lsp_mappings(ev)
+local function lsp_mappings(ev)
   local opts = { noremap = true, silent = true, buffer = ev.buf }
+
   keymap.set('n', 'grd', "<cmd>Trouble diagnostics<CR>", opts)
   keymap.set('n', 'grs', "<cmd>Trouble lsp_document_symbols<CR>", opts)
   keymap.set('n', 'gd', "<cmd>Trouble lsp_definitions<CR>", opts)
+  keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
 end
 
-function lsp_notification(ev)
+local function lsp_notification(ev)
   local client = vim.lsp.get_client_by_id(ev.data.client_id)
 
   vim.notify('LSP (' .. client.name .. '): Attached!', vim.log.levels.INFO)
@@ -42,13 +67,6 @@ function lsp_notification(ev)
       end
     end
   end
-end
-
--- Override the default hover function to use a custom border and size
--- global winborder breaks Telescope, so we use a custom hover function instead
-local hover = vim.lsp.buf.hover
-vim.lsp.buf.hover = function()
-  return hover({ border = "rounded" })
 end
 
 vim.api.nvim_create_autocmd('LspAttach', {
