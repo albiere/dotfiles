cb93f17ebba5a5078137b697d48b3bc6a521e0b1
Angelo Albiero Neto <angeloalbiero@hey.com>
2025-06-25 01:30:14 -0400

    Make nvim-tree a centered floater

--- nvim/dot-config/nvim/lua/albiere/plugins/nvim-tree.lua
+++ nvim/dot-config/nvim/lua/albiere/plugins/nvim-tree.lua
@@ -3,6 +3,21 @@ if not setup then
   return
 end
 
+local function open_win_config_func()
+  local scr_w = vim.opt.columns:get()
+  local scr_h = vim.opt.lines:get()
+  local tree_w = 80
+  local tree_h = math.floor(tree_w * scr_h / scr_w)
+  return {
+    border = "rounded",
+    relative = "editor",
+    width = tree_w,
+    height = tree_h,
+    col = (scr_w - tree_w) / 2,
+    row = (scr_h - tree_h) / 2
+  }
+end
+
 -- recommended settings from nvim-tree documentation
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -11,6 +26,13 @@ vim.g.loaded_netrwPlugin = 1
 vim.cmd([[ highlight NvimTreeIndentMarker guifg=#ff966c ]])
 
 nvimtree.setup({
+  view = {
+    signcolumn = "no",
+    float = {
+      enable = true,
+      open_win_config = open_win_config_func
+    },
+  },
   renderer = {
     icons = {
       glyphs = {
