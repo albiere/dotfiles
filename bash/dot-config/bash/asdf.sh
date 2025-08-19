a9a1538b7ffafa13ab26cb85864aec6b1319dfad
Angelo Albiero Neto <angeloalbiero@hey.com>
2025-08-19 09:56:24 -0400

    Configure asdf with latest recommendations

--- bash/dot-config/bash/asdf.sh
+++ bash/dot-config/bash/asdf.sh
@@ -1,8 +1,4 @@
-source $HOME/.config/asdf/asdf.sh
-source $HOME/.config/asdf/completions/asdf.bash
-
 export ASDF_DIR=$HOME/.config/asdf
 export ASDF_DATA_DIR=$HOME/.config/asdf
 
-export PATH="/usr/local/opt/asdf/bin:$PATH"
-export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+. <(asdf completion bash)
