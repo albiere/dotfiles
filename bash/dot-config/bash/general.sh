938e8ad98d9b8419101577455ae62e675f88c9d4
Angelo Albiero Neto <angeloalbiero@hey.com>
2025-08-19 10:00:10 -0400

    Change autojump directory to new homebrew apple silicon installation

--- bash/dot-config/bash/general.sh
+++ bash/dot-config/bash/general.sh
@@ -17,4 +17,4 @@ if [ -f $(brew --prefix)/etc/bash_completion ]; then
   . $(brew --prefix)/etc/bash_completion
 fi
 
-[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
+[ -f /opt/homebrew/etc/profile.d/autojump.sh ] && . /opt/homebrew/etc/profile.d/autojump.sh
