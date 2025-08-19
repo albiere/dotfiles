de8f0dcc7d692f16c67b066f90ef0000f75665fd
Angelo Albiero Neto <angeloalbiero@hey.com>
2025-08-19 14:21:16 -0400

    Update macos settings

--- bash/dot-config/bash/osx.sh
+++ bash/dot-config/bash/osx.sh
@@ -9,3 +9,16 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Avoid creating .DS_Store files on network volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Shorter initial delay before repeating (about 200ms)
+defaults write -g InitialKeyRepeat -int 15
+
+# Faster repeat rate (about 30ms between repeats)
+defaults write -g KeyRepeat -int 2
+
+# Enable tap-to-click for trackpad
+defaults -currentHost write -globalDomain com.apple.mouse.tapBehavior -int 2
+defaults write -g com.apple.mouse.tapBehavior -int 2
+
+# Enable Reduce Motion
+defaults write com.apple.universalaccess reduceMotion -bool true
